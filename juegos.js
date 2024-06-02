fcc18d743ec5aa62c2a8feb83dc53dee8963f188
Esteban <es.alamos@duocuc.cl>
2024-06-01 20:06:29 -0400

    api de nintendo lista

---
+++ juegos.js
@@ -0,0 +1,71 @@
+
+const juegos = [
+    {
+        "id": "1",
+        "name": "Monopoly",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1639696447-monopoly-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "5.000",
+        "cantidad": 50,
+        "descripcion": "Es un juego que combina la suerte y la estrategia, y se ha convertido en uno de los juegos de mesa más populares y conocidos en todo el mundo, ofreciendo horas de diversión y competencia entre amigos y familiares."
+    },   
+    {
+        "id": "2",
+        "name": "Uno",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1639871825-uno-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "5.000",
+        "cantidad": 40,
+        "descripcion": "Uno combina la suerte del sorteo con la estrategia de jugar cartas en el momento adecuado. Las cartas de acción y los comodines añaden un elemento de sorpresa y cambian constantemente el flujo del juego, haciendo que sea emocionante y dinámico."
+    },
+    {
+        "id": "3",
+        "name": "Gang Beast",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1660175823-gang-beasts-nintendo-switch-0.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "6.000",
+        "cantidad": 35,
+        "descripcion": "Es un juego de lucha divertido y caótico que destaca por su física torpe y humorística, sus variados escenarios peligrosos y su enfoque en el multijugador."
+    },
+    {
+        "id": "4",
+        "name": "Overcooked 2",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1640129689-overcooked-2-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "10.000",
+        "cantidad": 19,
+        "descripcion": "Es un juego divertido y frenético que desafía a los jugadores a colaborar y comunicarse eficazmente para gestionar cocinas caóticas y entregar pedidos a tiempo. "
+    },
+    {
+        "id": "5",
+        "name": "Dragon Ball FighterZ",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1641943131-dragon-ball-fighterz-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "20.000",
+        "cantidad": 24,
+        "descripcion": "Dragon Ball FighterZ es un videojuego de lucha en 2D1desarrollado por Arc System Works y distribuido por Bandai Namco Entertainment, el juego combina personajes tridimensionales con diferentes planos para ofrecer una acción clavada a la del anime y el manga."
+    },
+    {
+        "id": "6",
+        "name": "Ea Sports Fc 24",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1689293061-ea-sports-fc-24-nintendo-switch-pre-orden-0.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "18.000",
+        "cantidad": 60,
+        "descripcion": "EA SPORTS FC 24 es un nuevo modo de juego de fútbol. Incluye más de 19,000 jugadores totalmente licenciados, 700 equipos y más de 30 ligas. El modo Ultimate Team incluye imágenes más grandes y fondos animados, escudos de ligas y PlayStyles"
+    },
+    {
+        "id": "7",
+        "name": "Captain Tsubasa Rise Of New Champions",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1637886025-captain-tsubasa-rise-of-new-champions-nintendo-switch.jpg",
+        "consola": "Nintendo Switch",
+        "precio": "10.000",
+        "cantidad": 28,
+        "descripcion": "combina la acción rápida y emocionante del fútbol arcade con la nostalgia y el estilo del amado anime, ofreciendo una experiencia única tanto para los fans del fútbol como para los seguidores de la serie Captain Tsubasa"
+    }
+
+];
+
+module.exports = {
+    juegos
+} 
